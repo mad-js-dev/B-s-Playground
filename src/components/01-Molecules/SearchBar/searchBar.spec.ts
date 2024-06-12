fd20e9adf238fdd3b4c70a0dd565ef3b60b7f77f
Brais <braisva@gmail.com>
2024-06-12 15:41:55 +0200

    Searchbar Base TDD test

---
+++ src/components/01-Molecules/SearchBar/searchBar.spec.ts
@@ -0,0 +1,56 @@
+import { mount } from 'cypress/vue'
+import SearchBar from './SearchBar.vue'
+import SvgIcon from 'vue3-icon'
+
+describe('<SearchBar />', () => {
+  const testLabel = 'Search'
+
+  it('Has a search leading icon', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('Has a search input', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('Has a "search" by default as placeholder', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('has a custom placeholder prop', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('When its state != "" has a trailing cross icon', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('When trailing cross icon is clicked state is reset to ""', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+
+  it('emits a change event every time a key is pressed while input is focused', () => {
+    mount(SearchBar, {
+      propsData: {}
+    })
+    cy.get('.foo').should('have.class', 'bar')
+  })
+})
